28a1ca436d535704c275064569d9f091c2b66bcc
Kenny Halim <kennyhalim98@gmail.com>
2019-01-18 12:16:05 -0800

    add image when showing the results, add fadein effect as well

--- js/scripts.js
+++ js/scripts.js
@@ -16,21 +16,49 @@ $(document).ready(function(){
 
     if (question1 === "yes1") {
       if (question2 === "frontend" || question3 === "website") {
-        alert("CSS/Design");
+        $(".name").text(name1);
+        $("#output").text("CSS/Design!");
+        $("p#output").after('<img src="img/css.png"/>');
+        $("#result").fadeIn();
+        $("#quiz").hide();
       } else if (question2 === "backend" && question3 === "mobile") {
-        alert("Mobile/Java");
+        $(".name").text(name1);
+        $("#output").text("Java/Android!");
+        $("p#output").after('<img src="img/java.png"/>');
+        $("#result").fadeIn();
+        $("#quiz").hide();
       } else if (question2 === "backend" && question3 === "software" && question4 === "yes4" || question5 === "microsoft") {
-        alert("C#");
+        $(".name").text(name1);
+        $("#output").text("C#/.NET!");
+        $("p#output").after('<img src="img/csharp.png"/>');
+        $("#result").fadeIn();
+        $("#quiz").hide();
       } else if (question2 === "both" || question2 === "backend" && question3 === "software" && question4 ==="no4" || question5 ==="twitter") {
-        alert("Ruby");
+        $(".name").text(name1);
+        $("#output").text("Ruby/Rails!");
+        $("p#output").after('<img src="img/ruby.png"/>');
+        $("#result").fadeIn();
+        $("#quiz").hide();
       } else {
-        alert("PHP");
+        $(".name").text(name1);
+        $("#output").text("PHP/Drupal!");
+        $("p#output").after('<img src="img/php.png"/>');
+        $("#result").fadeIn();
+        $("#quiz").hide();
       }
     } else {
-      alert("toobad");
+      $("#quiz").hide();
+      $("#sorry").show();
     }
     event.preventDefault();
 
+  });
+
+  $("button#goBack").click(function(){
+    $(".intro").show();
+    $("#result").hide();
+    $("#sorry").hide();
+    location.reload();
   });
 
 
